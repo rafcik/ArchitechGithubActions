00cf6d95b3c42805642b5b712fa6389ceeb106ba
rafcik <pasiutrafal@gmail.com>
2020-06-09 22:07:46 +0200

    initial commit with created workspace and updated app

--- apps/github-actions-e2e/src/integration/app.spec.ts
+++ apps/github-actions-e2e/src/integration/app.spec.ts
@@ -8,6 +8,6 @@ describe('github-actions', () => {
     cy.login('my-email@something.com', 'myPassword');
 
     // Function helper example, see `../support/app.po.ts` file
-    getGreeting().contains('Welcome to github-actions!');
+    getGreeting().contains('Architech');
   });
 });
--- apps/github-actions/src/app/app.component.spec.ts
+++ apps/github-actions/src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
@@ -14,18 +14,12 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'github-actions'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('github-actions');
-  });
-
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to github-actions!'
+      'Architech'
     );
   });
 });
